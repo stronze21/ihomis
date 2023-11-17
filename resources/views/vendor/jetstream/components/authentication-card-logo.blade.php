c4a4b75eaa0a05f2cf8cddf2440a5cace3418b77
stronze21 <57517246+stronze21@users.noreply.github.com>
2023-11-17 14:07:48 +0800

    2023-11-17

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,7 @@
-<a href="/">
-    <img src="{{asset('logo.png')}}" alt="MMMHMC Logo">
-</a>
+<div class="flex flex-col justify-center space-y-3">
+    <a href="/" class="mx-auto">
+        <img src="{{asset('logo.png')}}" alt="MMMHMC Logo">
+    </a>
+
+    <span class="text-2xl font-bold">{{ env('APP_NAME') }}</span>
+</div>
