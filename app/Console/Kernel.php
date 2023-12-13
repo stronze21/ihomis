36246873955b2b8532933552e232b1f9a2b7cf7f
Martin Joshua Garaza <joshua070915@gmail.com>
2023-12-13 13:50:00 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->everyMinute();
-        $schedule->command('init:stock-card')->daily();
+        $schedule->command('init:stock-card')->everyMinute();
     }
 
     /**
