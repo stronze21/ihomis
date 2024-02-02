9b3863b8f0d2c7996239593ddb97e4440d11dc79
ihomp <dts.mmmh@gmail.com>
2024-02-02 08:06:52 +0800

    Update rxo-charge-slip.blade.php

--- resources/views/livewire/pharmacy/dispensing/rxo-charge-slip.blade.php
+++ resources/views/livewire/pharmacy/dispensing/rxo-charge-slip.blade.php
@@ -94,7 +94,7 @@
                 </tfoot>
             </table>
             <div class="flex flex-col py-0 my-0 text-left text-xs/4 whitespace-nowrap">
-                <div>Issued by: {{ $rxo_header->employee ? $rxo_header->employee->fullname() : $rxo_header->entry_by }}{{ $rxo_header->entry_by }}
+                <div>Issued by: {{ $rxo_header->employee ? $rxo_header->employee->fullname() : $rxo_header->entry_by }}
                 </div>
                 <div><span>Time: {{ \Carbon\Carbon::create($rxo_header->dodate)->format('h:i A') }}</span></div>
                 <div><span>Verified by Nurse/N.A.: _________________________</span></div>
