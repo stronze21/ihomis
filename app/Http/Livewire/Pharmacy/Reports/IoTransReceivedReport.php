c68692fd62128200bdec1f635b5c285d2dc8dac2
Martin Joshua Garaza <joshua070915@gmail.com>
2024-03-25 09:12:21 +0800

    03-25-2024

--- app/Http/Livewire/Pharmacy/Reports/IoTransReceivedReport.php
+++ app/Http/Livewire/Pharmacy/Reports/IoTransReceivedReport.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Pharmacy\Reports;
 
 use App\Models\Pharmacy\Drugs\InOutTransaction;
+use App\Models\References\ChargeCode;
 use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -12,22 +13,31 @@ class IoTransReceivedReport extends Component
 
     use WithPagination;
 
-    public $from, $to, $search;
+    public $from, $to, $search, $filter_charge = 'DRUME';
 
     public function render()
     {
         $from = Carbon::parse($this->from)->startOfDay();
         $to = Carbon::parse($this->to)->endOfDay();
 
+        $charge_codes = ChargeCode::where('bentypcod', 'DRUME')
+            ->where('chrgstat', 'A')
+            ->whereIn('chrgcode', array('DRUMA', 'DRUMB', 'DRUMC', 'DRUME', 'DRUMK', 'DRUMAA', 'DRUMAB', 'DRUMR', 'DRUMS', 'DRUMAD', 'DRUMAE', 'DRUMAF', 'DRUMAG'))
+            ->get();
+
         $trans = InOutTransaction::where('trans_stat', 'Received')
             ->where('loc_code', session('pharm_location_id'))
             ->whereBetween('updated_at', [$from, $to])
+            ->whereHas('item', function ($query) {
+                $query->where('chrgcode', $this->filter_charge);
+            })
             ->with('drug')
             ->with('location')
             ->get();
 
         return view('livewire.pharmacy.reports.io-trans-received-report', [
             'trans' => $trans,
+            'charge_codes' => $charge_codes,
         ]);
     }
 
